e14d9483ab199add826fe556c315df4123c699e6
billdigaetano <digaetanobill@gmail.com>
2025-06-05 19:17:18 -0500

    Update: wow homepage styling and branding

--- src/App.js
+++ src/App.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  );
-}
-
-export default App;
+// App.js with WOW updates
